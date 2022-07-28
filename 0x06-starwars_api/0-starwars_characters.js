b6d22092371041f978b2dfee6a043dd103029a67
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-29 00:23:50 +0300

    render name

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,25 @@
+#!/usr/bin/node
 
+const request = require('request');
+
+const id = process.argv[2] + '/';
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url + id, async function (err, res, body) {
+    if (err) return console.error(err);
+
+    const list_url = JSON.parse(body).characters;
+
+
+    for (const li of list_url) {
+        await new Promise(function (resolve, reject) {
+            request(li, function (err, res, body) {
+                if (err) return console.error(err);
+
+              ]
+                console.log(JSON.parse(body).name);
+                resolve();
+            });
+        });
+    }
+});
