af7ae0f5d24935ba50b5f36e75b923ec4801db87
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-31 07:57:29 +0300

    modify for style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,20 +6,18 @@ const id = process.argv[2] + '/';
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
 request(url + id, async function (err, res, body) {
-    if (err) return console.error(err);
+  if (err) return console.error(err);
 
-    const list_url = JSON.parse(body).characters;
+  const listUrl = JSON.parse(body).characters;
 
+  for (const li of listUrl) {
+    await new Promise(function (resolve, reject) {
+      request(li, function (err, res, body) {
+        if (err) return console.error(err);
 
-    for (const li of list_url) {
-        await new Promise(function (resolve, reject) {
-            request(li, function (err, res, body) {
-                if (err) return console.error(err);
-
-              
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
